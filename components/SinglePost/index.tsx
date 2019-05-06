b71b50e5c737a6e71a8d6c58005904f40d649223
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-05-06 21:58:10 +0200

    Add go-back arrow in single post page

--- components/SinglePost/index.tsx
+++ components/SinglePost/index.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import Link from "next/link";
 import {IPost} from "../Blog/types";
 
 export const SinglePost: React.FC<IPost> = React.memo(({title, content}) => {
   return (
-    <article>
-      <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{__html: content}} />;
-    </article>
+    <>
+      <article>
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{__html: content}} />
+      </article>
+
+      <div css={{ marginTop: "1.5rem" }}>
+        <Link href="/blog">
+          <a>
+            <i className="fas fa-arrow-left"></i>
+            <span css={{ marginLeft: ".7rem" }}>Blog</span>
+          </a>
+        </Link>
+      </div>
+    </>
   )
 });
 
