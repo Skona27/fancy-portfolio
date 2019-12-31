3a674b46bd351d21f050d5168af3562dc84bfd57
Jakub Skoneczny <skoneczny.j@gmail.com>
2019-12-31 19:02:51 +0100

    order posts by date

--- api/blog.ts
+++ api/blog.ts
@@ -5,7 +5,7 @@ export const getAllPosts = async (): Promise<IPost[]> => {
   // Dane z Firebase przychodzą w dziwnym JSON-nie
   // Próbujemy przemapować na YOLO, jak będzie źle to rzucamy błąd!
   try {
-    const result = await api.get("/posts");
+    const result = await api.get("/posts?oderBy='$date'");
     return result.data.documents.map((el: any) => {
       return {
         id: el.name.split("/posts/")[1],
