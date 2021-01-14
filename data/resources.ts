ae3f3d8b77d89d6ea62f6768a05228f3609677a3
Jakub Skoneczny <skoneczny.j@gmail.com>
2021-01-14 21:05:34 +0100

    feat: add resources

--- data/resources.ts
+++ data/resources.ts
@@ -35,6 +35,53 @@ const universalResources: LinkResource[] = [
     description:
       "Series of tutorials on how to build a game from scratch with TypeScript and native browser APIs",
   },
+  {
+    type: "LINK",
+    title: "Create Your Own Compiler",
+    href: "https://citw.dev/tutorial/create-your-own-compiler",
+    description:
+      "The Super Tiny Compiler is a simple compiler written in Javascript and we'll write it step by step from scratch",
+  },
+  {
+    type: "LINK",
+    title: "Using React + Google Sheets as your CMS",
+    href:
+      "https://medium.com/@ryan.mcnierney/using-react-google-sheets-as-your-cms-294c02561d59",
+    description:
+      "Tutorial on how to serve up any data from a Google Sheet into a real React application",
+  },
+  {
+    type: "LINK",
+    href:
+      "https://developer.mozilla.org/en-US/docs/Web/API/Web_Speech_API/Using_the_Web_Speech_API",
+    title: "Using the Web Speech API",
+    description:
+      "The Web Speech API enables you to incorporate voice data into web apps",
+  },
+  {
+    type: "LINK",
+    title: "Continuous Deployment for Node.js on the Google Cloud Platform",
+    href:
+      "https://medium.com/free-code-camp/continuous-deployment-for-node-js-on-google-cloud-platform-751a035a28d5",
+    description:
+      "Setup your application to deploy to App Engine on GitHub push",
+  },
+  {
+    type: "LINK",
+    title: "Dockerizing and autoscaling Node.js on Google Cloud",
+    href:
+      "https://levelup.gitconnected.com/dockerizing-and-autoscaling-node-js-on-google-cloud-ef8db3b99486",
+    description:
+      "Learn how to dockerize a Node.js server for hosting in Google Cloud and configuring your VM instances to auto scale proportionally with traffic",
+  },
+  {
+    type: "LINK",
+    href:
+      "https://medium.com/@louiskengo3/getting-started-with-terraform-and-google-cloud-platform-9c3c45dc5bde",
+    title: "Getting Started with Terraform and Google Cloud Platform",
+    description:
+      "Terraform is an automation tool used to ease the process of setting up infrastructure on which you can deploy applications",
+  },
 ];
 
 export const resources: Record<Languages, ResourceGroup[]> = {
